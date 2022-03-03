5fda6d4c0928d0d3ff0ed89d0dbb2227182183d3
stevey d <stphndcky22@gmail.com>
2022-03-03 15:18:47 -0800

    created dynamic fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,21 @@
 // script file for git-it-done
 
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+// passing user into function will return data from many users, DYNAMIC
+var getUserRepos = function (user) { 
+
+    // define the url variable with user parameter
+    var url = "https://api.github.com/users/" + user + "/repos";
+
+    // we fetch data, use .then method on promise, capture response in callback function
+    fetch(url).then(function(response) {
+        // response must be reformatted via json(), this we receive as promise which must have .then method following
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+
+ 
 };
+
 
 getUserRepos();
